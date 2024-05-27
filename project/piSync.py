c778d819ce21a7741070fb0efed606b65ce5170c
thepinkfluffy <dioszegizoltan@yahoo.com>
2024-05-27 21:03:50 +0300

    stuff

--- project/piSync.py
+++ project/piSync.py
@@ -9,13 +9,13 @@ except:
 condition = sys.argv[1]
 try:
 	if(condition  == "upload"):
-		mystr = ("scp -r -p /home/diosz/Desktop/project/ pi@"+str(ip)+":/home/pi/")
+		mystr = ("scp -r -p /home/diosz/Desktop/python-stepper-goto/project/ pi@"+str(ip)+":/home/pi/")
 		os.system(mystr)
 	elif (condition == "download"):
 		a = input("Warning!The following action will OVERWRITE local files. Do you wish to proceed?")
 		if(a!="yes" or a!="y" or a!= "Yes" or a!="YES"):
 			exit()
-		mystr = ("scp -r -p pi@"+str(ip)+":/home/pi/project /home/diosz/Desktop/")
+		mystr = ("scp -r -p pi@"+str(ip)+":/home/pi/project /home/diosz/Desktop/python-stepper-goto")
 		os.system(mystr)
 except:
 	print("usage: python3 piSync.py upload \n python3 piSync.py download")
